d19e14ad8c681a302b109203b2a6538a32a7fb7c
Bongjun <bongjun.jang@kaist.ac.kr>
2025-11-23 18:13:22 +0900

    update release

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 gh release create v20.1.1-5.2.0 \
-  ./llvm-20.1.1-ocaml-5.2.0-linux-x86_64.tar.gz \
+  ./llvm-20.1.1-ocaml-5.2.0.tar.gz \
   --repo prosyslab/llvm-builds \
   --title "LLVM 20.1.1 for X86/Wasm + OCaml 5.2.0 + RTTI" \
   --notes "Clang, LLD, and OCaml bindings for X86 and WebAssembly"
