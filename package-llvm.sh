19dd5c04b1210bc79151abb39ceca0586e41fea7
Bongjun <bongjun.jang@kaist.ac.kr>
2025-11-23 09:10:37 +0000

    update packaging

--- package-llvm.sh
+++ package-llvm.sh
@@ -9,5 +9,17 @@ test -d $BUILT
 or exit 1
 
 echo "Packaging $BUILT for OPAM"
+pushd $BUILT/lib
+
+# `dune build` searches LLVM libraries in `switch-prefix/lib/llvm` via `ocamlfind`
+# but the libraries are installed in `switch-prefix/lib`.
+# To aid the dune's build system via `ocamlfind`, we create symlinks.
+for llvmLib in libLLVM*.a
+  pushd llvm/
+  ln -sf ../$llvmLib .
+  popd
+end
+
+popd
 
 tar -czvf "llvm-20.1.1-ocaml-5.2.0.tar.gz" $BUILT
